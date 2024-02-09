c806fad49dc2ac5f79f10bc71483787108ee30d3
imanmaknojia <maknojiaiman@gmail.com>
2024-02-08 18:44:25 -0700

    update

--- js/google-map.js
+++ js/google-map.js
@@ -4,10 +4,8 @@ var google;
 function init () {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng( 22.5726, 88.3639 );
-    // 39.399872
-    // -8.224454
+    var myLatlng = new google.maps.LatLng(33.4255, -111.9400);
+
 
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
@@ -43,7 +41,7 @@ function init () {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map( mapElement, mapOptions );
 
-    var addresses = [ 'Kolkata' ];
+    var addresses = [ 'Tempe' ];
 
     for ( var x = 0; x < addresses.length; x++ ) {
         $.getJSON( 'http://maps.googleapis.com/maps/api/geocode/json?address=' + addresses[ x ] + '&sensor=false', null, function ( data ) {
